401bde73fcd47523f5998a939162c1ebca3174a7
amardeep0851 <amardeep0851@gmail.com>
2025-12-30 08:05:13 +0000

    change in watermark style

--- src/components/watermark/watermark.tsx
+++ src/components/watermark/watermark.tsx
@@ -1,15 +1,17 @@
 export default function Watermark() {
   return (
     <div>
-      <div className="text-center text-xl">
-        Preview environment — content subject to change
+      <div className="fixed top-0 left-0 w-full z-[9999] pointer-events-none">
+      <div className="bg-red-700 text-white py-1 px-4 shadow-md flex justify-center items-center pointer-events-auto border-b border-yellow-600">
+        <div className="flex items-center space-x-2 text-xs md:text-sm font-bold tracking-tight uppercase">
+          <span className="bg-black text-white px-2 py-0.5 rounded text-[10px]">Staging</span>
+          <p>
+            Preview environment — All content subject to change.
+          </p>
+        </div>
       </div>
-    {/* <div className="pointer-events-none fixed bottom-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-50 opacity-80">
-      <span className="md:text-6xl text-4xl uppercase tracking-wider text-gray-400 ">
-        Preview • Not Final
-      </span>
-    </div> */}
-
+    </div>
+    <div className="mb-7" />
     </div>
   );
 }
